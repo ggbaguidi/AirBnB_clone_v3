9ca8d60509e3aa50bdc8919906e1440879dadaba
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-28 21:35:09 +0100

    chore: make file executables

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,4 +6,5 @@ from api.v1.views import app_views
 @app_views.route("/status", strict_slashes=False)
 def get_status():
     """get status"""
+    print("Reached the /status route")
     return jsonify({"status": "OK"})
