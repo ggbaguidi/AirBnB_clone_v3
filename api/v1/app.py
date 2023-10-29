24d6c503b4f0d076c6b7cadb725611a9876f6efa
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-29 10:39:57 +0100

    fix: fix error about the third requirement

--- api/v1/app.py
+++ api/v1/app.py
@@ -30,5 +30,5 @@ def page_not_found(error):
 
 if __name__ == "__main__":
     HBNB_API_HOST = environ.get("HBNB_API_HOST", "0.0.0.0")
-    HBNB_API_PORT = environ.get("HBNB_API_PORT", 5000)
+    HBNB_API_PORT = int(environ.get("HBNB_API_PORT", 5000))
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
