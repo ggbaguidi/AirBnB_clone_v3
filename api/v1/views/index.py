25f8995c5bd437711ef0441d7bc9129473ca0491
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-29 07:58:05 +0100

    feat: Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,29 @@
 """entry of api"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
 def get_status():
     """get status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def get_stats():
+    """Create an endpoint that retrieves the number of each objects by type"""
+    return jsonify({
+    "amenities": storage.count(Amenity), 
+    "cities": storage.count(City), 
+    "places": storage.count(Place), 
+    "reviews": storage.count(Review), 
+    "states": storage.count(State), 
+    "users": storage.count(User)
+})
