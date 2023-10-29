bf6d7306af14078f5ad37dc5d084aa6df62fe706
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-29 06:38:19 +0100

    perf: import all routes in index.py to __init__.py module

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def get_status():
     """get status"""
     return jsonify({"status": "OK"})
